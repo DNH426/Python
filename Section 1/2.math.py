c7cd6e13ec8dab2bb109d6dc6b9d65d390bd7814
dhr2505 <daniel.hagy@northwestern.edu>
2020-08-06 15:13:45 -0500

    foundation

---
+++ Section 1/2.math.py
@@ -0,0 +1,14 @@
+# Add
+print(2+1)
+# Multiply
+print(2*1)
+# Divide
+print(4/2)
+# Modulo or "Mod" Operator
+print(7%4)
+# Powers
+print(2**3)
+# Order of ope
+print(2+10*10+3)
+# Order of ope
+print((2+10)*10+3)
---
+++ Section 1/3.variables.py
@@ -0,0 +1,46 @@
+# Global var are in CAPS
+# Python uses Dynamic Typing: Gives the developer the ability to reassign variables to different data types
+# Pros of Dynamic Typing:
+    # Very easy to work with
+    # Faster development time
+# Cons of Dynamic Typing:
+    # May result in bugs for unexpected data types
+    # Need to be aware of type()
+
+# ========================
+
+# Eaxmples
+
+# 1
+
+# data type = int 
+# my_dogs = 2
+
+# data type = list
+# my_dogs = ["Cubs", "Sox"]
+
+# 2
+
+# What will be logged?
+# a = 5
+# a = 'dog'
+# print(a)
+# print(a)
+# Dog because the last data type assigned to a was 'dog'
+
+# 3
+
+# a = 10
+# print(a)
+# a = 6
+# a = a + a
+# print(a)
+# print(type(a))
+
+# 4
+
+# my_income = 150
+# tax_rate = 0.1
+# my_taxes = my_income * tax_rate
+
+# print(my_taxes)
